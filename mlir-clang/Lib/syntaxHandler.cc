4951da9945f1a8d1335222e326e3e72c44f21850
Lorenzo Chelini <l.chelini@icloud.com>
2021-09-08 20:51:04 +0200

    yaml export

--- mlir-clang/Lib/syntaxHandler.cc
+++ mlir-clang/Lib/syntaxHandler.cc
@@ -1,23 +1,57 @@
 #include "syntaxHandler.h"
 #include "clang/Parse/Parser.h"
+#include "llvm/Support/ToolOutputFile.h"
+#include "llvm/Support/YAMLTraits.h"
 
 using namespace clang;
 
+using llvm::yaml::MappingTraits;
+using llvm::yaml::Output;
+
 namespace {
 
+struct LinalgPlugin {
+  std::string funcName;
+  std::string body;
+};
+
+} // namespace
+
+namespace llvm {
+namespace yaml {
+
+template <> struct MappingTraits<LinalgPlugin> {
+  static void mapping(IO &io, LinalgPlugin &info) {
+    io.mapRequired("funcName", info.funcName);
+    io.mapRequired("body", info.body);
+  }
+};
+
+} // namespace yaml
+} // namespace llvm
+
+namespace {
+
+// XXX: The llvm registry requires registered subclass to have a default
+// constructor.
+// But we need to export the information parsed here. For now write them on a
+// file.
 class PrintTokensHandler : public SyntaxHandler {
 public:
   PrintTokensHandler() : SyntaxHandler("tokens") {}
 
   void GetReplacement(Preprocessor &PP, Declarator &D, CachedTokens &Toks,
                       llvm::raw_string_ostream &OS) override {
-    StringRef ti = D.getIdentifier()->getName();
-    llvm::errs() << ti << "\n";
+    std::string funcName = D.getIdentifier()->getName().str();
+
     std::string content = "";
     for (auto &Tok : Toks)
       content += PP.getSpelling(Tok);
     content += "\n";
-    llvm::errs() << content << "\n";
+    LinalgPlugin p = {funcName, content};
+
+    Output yout(llvm::outs());
+    yout << p;
   }
 
   void AddToPredefines(llvm::raw_string_ostream &OS) override {}
--- mlir-clang/Test/polybench/linear-algebra/kernels/mvt/mvt.c
+++ mlir-clang/Test/polybench/linear-algebra/kernels/mvt/mvt.c
@@ -95,17 +95,11 @@ kernel_mvt(int n, DATA_TYPE POLYBENCH_1D(x1, N, n),
            DATA_TYPE POLYBENCH_1D(x2, N, n), DATA_TYPE POLYBENCH_1D(y_1, N, n),
            DATA_TYPE POLYBENCH_1D(y_2, N, n),
            DATA_TYPE POLYBENCH_2D(A, N, N, n, n)) {
-  /*
-    int i, j;
-
-  #pragma scop
-    for (i = 0; i < _PB_N; i++)
-      for (j = 0; j < _PB_N; j++)
-        x1[i] = x1[i] + A[i][j] * y_1[j];
-  #pragma endscop
-  */
-
-  x1(i) += A(i, j) * y_1(j) x2(i) += A(j, i) * y_2(j)
+  // clang-format off
+  x1(i) += A(i, j) * y_1(j) 
+  
+  x2(i) += A(j, i) * y_2(j)
+  // clang-format on
 }
 
 int main(int argc, char** argv)
